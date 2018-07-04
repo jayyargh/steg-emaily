e9ae4b16800e4990c291e3401899ced8ad9e7e04
Justin Rigler <justinmrigler@gmail.com>
2018-07-04 15:22:34 -0400

    fixed greeting

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-  res.send({ users: 'test' });
+  res.send({ bye: 'friends' });
 });
 
 const PORT = process.env.PORT || 5000;
